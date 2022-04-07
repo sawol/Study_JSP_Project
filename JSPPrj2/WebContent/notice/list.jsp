0b0b67c6acba9434c83f90ed3ab7732acb0332b0
sawol <55649302+sawol@users.noreply.github.com>
2022-04-08 01:49:13 +0900

    목록 페이지 MVC2 적용

--- JSPPrj2/WebContent/notice/list.jsp
+++ JSPPrj2/WebContent/notice/list.jsp
@@ -1,19 +1,8 @@
-<%@page import="java.sql.ResultSet"%>
-<%@page import="java.sql.Statement"%>
-<%@page import="java.sql.DriverManager"%>
-<%@page import="java.sql.Connection"%>
+<%@page import="com.newlecture.web.entity.Notice"%>
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
     
-<%
-String url = "jdbc:mysql://localhost:3306/jsppj?serverTimezone=UTC";
-String sql = "SELECT * FROM NOTICE";
-
-Class.forName("com.mysql.jdbc.Driver");
-Connection con = DriverManager.getConnection(url, "ssafy", "ssafy");
-Statement st = con.createStatement();
-ResultSet rs = st.executeQuery(sql);
-%>
 <!DOCTYPE html>
 <html>
 
@@ -186,22 +175,24 @@ ResultSet rs = st.executeQuery(sql);
 						</tr>
 					</thead>
 					<tbody>
+					
 					<% 
-					while(rs.next()){
-					%>
-						
-					<tr>
-						<td><%=rs.getInt("ID")%></td>
-						<td class="title indent text-align-left"><a href="detail?id=<%=rs.getInt("ID")%>"><%=rs.getString("Title")%></a></td>
-						<td><%=rs.getString("writer_id")%></td>
-						<td>
-							<%=rs.getDate("regdate")%>		
-						</td>
-						<td><%=rs.getInt("hit")%></td>
-					</tr>
-					<%				
-					}
+					List<Notice> list = (List<Notice>)request.getAttribute("list");
+					for(Notice n : list){
+						pageContext.setAttribute("n", n);
 					%>	
+					<tr>
+						<td>${n.id}</td>
+						<td class="title indent text-align-left"><a href="detail?id=${n.id}"></a>${n.title}</td>
+						<td>${n.writerId}</td>
+						<td>
+							${n.regdate}	
+						</td>
+						<td>${n.hit}</td>
+					</tr>
+					<%
+					}
+					%>		
 					</tbody>
 				</table>
 			</div>
@@ -274,8 +265,3 @@ ResultSet rs = st.executeQuery(sql);
     </body>
     
 </html>
-<%
-rs.close();
-st.close();
-con.close();
-%>
---
+++ JSPPrj2/src/com/newlecture/web/controller/NoticeListController.java
@@ -0,0 +1,59 @@
+package com.newlecture.web.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.newlecture.web.entity.Notice;
+
+@WebServlet("/notice/list")
+public class NoticeListController extends HttpServlet{
+		@Override
+		protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+			String url = "jdbc:mysql://localhost:3306/jsppj?serverTimezone=UTC";
+			String sql = "SELECT * FROM NOTICE";
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection con = DriverManager.getConnection(url, "ssafy", "ssafy");
+				Statement st = con.createStatement();
+				ResultSet rs = st.executeQuery(sql);
+				List<Notice> list = new ArrayList<>();
+				while(rs.next()){
+					int id = rs.getInt("id");
+					String title = rs.getString("title");
+					Date regdate = rs.getDate("regdate");
+					String writerId = rs.getString("writer_id"); 
+					int hit = rs.getInt("hit");
+					String files = rs.getString("files"); 
+					String content = rs.getString("content");
+					
+					Notice notice = new Notice(id, title, regdate, writerId, hit, files, content);
+					list.add(notice);
+				}
+				request.setAttribute("list", list);
+
+				rs.close();
+				st.close();
+				con.close();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			
+			request.getRequestDispatcher("/notice/list.jsp").forward(request, response);
+		}
+}
